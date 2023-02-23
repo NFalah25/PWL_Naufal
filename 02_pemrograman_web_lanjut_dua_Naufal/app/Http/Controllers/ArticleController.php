ab32e6ef22e338d19a7178e023d76f28ee39508a
= Nfalah25 <= falahnaufal1@gmail.com>
2023-02-23 20:21:34 +0700

    first commit

---
+++ 02_pemrograman_web_lanjut_dua_Naufal/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ArticleController extends Controller{
+    public function article($id){
+        return "Halaman Artiker dengan Id $id";
+    }
+
+}
+
+?>
---
+++ 02_pemrograman_web_lanjut_dua_Naufal/resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Nama : Naufal Falah Wafiuddin <br>
+        Kelas : TI-2C <br>
+        Absen : 24 <br>
+        NIM : 2141720129
+    </p>
+</body>
+</html>
+
+
